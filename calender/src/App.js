489b1f94957533fd1106e95ec1aa9a6ec873072e
Rahul <Rahul.Jaiswal@bentley.com>
2025-01-31 11:26:45 +0530

    added calender

---
+++ calender/src/App.js
@@ -0,0 +1,6 @@
+import DayView from "./components/DayView";
+import "./styles.css";
+// code link: https://codesandbox.io/p/sandbox/calendar-day-view-forked-q2zctw?file=%2Fpublic%2Findex.html%3A10%2C18
+export default function App() {
+  return <DayView />;
+}
